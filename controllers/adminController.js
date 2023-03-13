9b0edd8cbc0c4c6ffcade3e9b7c0983db081078f
Raj Prajapati <87031647+rjfantusty@users.noreply.github.com>
2023-03-13 23:31:07 +0530

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -8,7 +8,36 @@ module.exports.renderLoginForm = async (req, res) => {
 };
 
 module.exports.dashboard = async (req, res) => {
-  return res.render("admin/dashboard");
+  const total_donors = await getCertificates({});
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  console.log(today);
+  const total_donors_today = await getCertificates({ time: { $gte: today } });
+
+  const weekAgo = new Date();
+  weekAgo.setDate(weekAgo.getDate() - 7);
+  console.log(weekAgo);
+  const total_donors_week = await getCertificates({
+    time: { $gte: weekAgo },
+  });
+
+  const monthAgo = new Date();
+  monthAgo.setDate(monthAgo.getDate() - 30);
+  console.log(monthAgo);
+  const total_donors_month = await getCertificates({
+    time: { $gte: monthAgo },
+  });
+
+  console.log(total_donors);
+  console.log(total_donors_today);
+
+  return res.render("admin/dashboard", {
+    totalDonors: total_donors.length,
+    totalDonorToday: total_donors_today.length,
+    totalDonorsWeek: total_donors_week.length,
+    totalDonorsMonth: total_donors_month.length,
+  });
 };
 
 module.exports.login = async (req, res) => {
